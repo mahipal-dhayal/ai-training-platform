a13e6d037e0413e0ebd1698885a6c5e90341d844
mahipal-dhayal <mahipaldhayal2002@gmail.com>
2025-08-05 09:10:42 +0530

    login signup added on login or signup page for better interactivity

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,5 +1,16 @@
 import AuthForm from '@/components/AuthForm';
+import Link from 'next/link';
 
 export default function LoginPage() {
-  return <AuthForm type="login" />;
+  return (
+    <div className="p-6">
+      <AuthForm type="login" />
+      <p className="text-center mt-4 text-sm text-gray-600">
+        New here?{' '}
+        <Link href="/signup" className="text-blue-600 underline">
+          Create an account
+        </Link>
+      </p>
+    </div>
+  );
 }
--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -1,5 +1,16 @@
 import AuthForm from '@/components/AuthForm';
+import Link from 'next/link';
 
 export default function SignupPage() {
-  return <AuthForm type="signup" />;
+  return (
+    <div className="p-6">
+      <AuthForm type="signup" />
+      <p className="text-center mt-4 text-sm text-gray-600">
+        Already have an account?{' '}
+        <Link href="/login" className="text-blue-600 underline">
+          Login here
+        </Link>
+      </p>
+    </div>
+  );
 }
